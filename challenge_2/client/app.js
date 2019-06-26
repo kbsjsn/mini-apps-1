c03b8f8a38cfeef8b7725ed0717f40acc0c6aa72
Kirk Shin <kirkshin@Kirks-MacBook-Air.local>
2019-06-26 08:49:52 -0700

    completed parsing incoming JSON obj

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,2 +1,23 @@
+import $ from '../node_modules/jquery';
 
+$(document).ready(function(){ 
+  $('form').on('submit', function(e) {
+    e.preventDefault(); //prevents refreshing when clicking submit button
 
+    var formData = {
+      'text' : $('textarea[name=jsonData]').val()
+
+    };
+
+    $.ajax({
+      method: 'POST', 
+      url: '/upload_json',  
+      data: formData,
+      dataType: 'json'
+    })
+      .done(function(data) {
+        console.log(data);
+      });
+  });
+
+});
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,36 @@
+//receive the submit data from the client (as JSON) and render it to csv
+// render the data onto the html and also present blank form
 
+const express = require('express');
+const path = require('path');
+const parser = require('body-parser');
+const app = express();
+const port = 3900;
+
+app.use(express.static('client'));
+app.use(parser.urlencoded({extended:false}));
+// app.get('/', function(req, res) {
+//   console.log('GET request successful');
+//   res.sendFile(path.join(__dirname + '/client/index.html'));
+//   res.status(200).end()
+// })
+
+app.post('/upload_json', (req, res) => {
+  var jsonStr = req.body.jsonData;
+  var removedRsJSON = jsonStr.split('\r').join('');
+  var removedNsJSON = removedRsJSON.split('\n').map(str => str.trim()).join('');
+  //got rid of ';' at the end of removedNsJSON
+  var jsonObj = JSON.parse(removedNsJSON.substring(0, removedNsJSON.length - 1));
+  console.log(jsonObj.children[0].children); //array inside 'Beth Jr'
+  // var jsonObj = JSON.parse(jsonStr);
+  console.log('POST request successful', jsonObj);
+  res.sendFile(path.join(__dirname + '/client/index.html'));
+  res.status(201).end();
+});
+
+// var jsonToCSV = function(jsonObj) {
+
+// }
+
+app.listen(port, () => console.log(`Listening to port ${port}`));
 
